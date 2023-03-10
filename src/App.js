50bb370852ecab22d545b2c39080f91a5516979b
Anton Askling <anton.askling@gmail.com>
2023-03-10 17:42:57 +0100

    add docstring as reminder of what variants are available in react-bootsrap.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import './App.css';
 import CustomButton from './components/CustomButton';
 import Header from './components/Header';
 
+/**
+ * Custom button component that supports the following variants from Bootstrap:
+ * primary, secondary, success, danger, warning, info, light, dark, link
+ *
+ * Props:
+ * - variant: string (one of the supported Bootstrap variants)
+ * - text: string (the text to display on the button)
+ */
+
 function App() {
   return (
     <div className="App">
