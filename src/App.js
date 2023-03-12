d5d09aa5a1712b53cab9d24a0aba19984713ece8
Anton Askling <anton.askling@gmail.com>
2023-03-12 18:35:30 +0100

    add loggedIn state and auth-/unAuthNavto NavBar

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
     <div className="App">
       <NavBar/>
       <Routes>
-        <Route exact path='/' element={<Home />} />
+        <Route path='/' element={<Home />} />
       </Routes>
     </div>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
@@ -7,6 +7,29 @@ import Navbar from 'react-bootstrap/Navbar';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 
 function NavBar() {
+    const [loggedIn, setLoggedIn] = useState(true);
+
+    
+    // navigation links shown if loggedIn
+    const authNav = (
+        <>
+            <Nav.Link href="#action1">Feed</Nav.Link>
+            <Nav.Link href="#action2">Groups</Nav.Link>
+            <Nav.Link href="#action2">Tasks</Nav.Link>
+            <Nav.Link href="#action2">Profile</Nav.Link>
+            <Nav.Link href="#action2">Logout</Nav.Link>
+        </>
+    )
+
+    // navigation links shown if !loggedIn
+    const unAuthNav = (
+        <>
+            <Nav.Link href="#action2">Log in</Nav.Link>
+            <Nav.Link href="#action2">Sign Up</Nav.Link>
+        </>
+    )
+
+
     return (
         <>
             <Navbar bg="light" expand="lg" className="mb-3">
@@ -25,8 +48,9 @@ function NavBar() {
                         </Offcanvas.Header>
                         <Offcanvas.Body>
                             <Nav className="justify-content-end flex-grow-1 pe-3">
-                                <Nav.Link href="#action1">Log in</Nav.Link>
-                                <Nav.Link href="#action2">Sign up</Nav.Link>
+
+                                {loggedIn ? authNav : unAuthNav}
+
                                 {/* <NavDropdown
                                     title="Dropdown"
                                     id={`offcanvasNavbarDropdown`}
