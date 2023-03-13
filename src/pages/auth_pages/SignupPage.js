087b77a3c5cfea52dcdad15c7d311f73cddc50b2
Anton Askling <anton.askling@gmail.com>
2023-03-13 01:46:50 +0100

    add basic form to signup page that logs the input on submit

--- src/pages/auth_pages/SignupPage.js
+++ src/pages/auth_pages/SignupPage.js
@@ -2,16 +2,41 @@ import React, { useRef } from 'react'
 import { Form, Button } from 'react-bootstrap'
 
 function SignupPage() {
-    
-    const usernameRef = useRef("")
-    const password1ref = useRef("")
-    const password2ref = useRef("")
+  const usernameRef = useRef(null);
+  const password1Ref = useRef(null);
+  const password2Ref = useRef(null);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const username = usernameRef.current.value;
+    const password1 = password1Ref.current.value;
+    const password2 = password2Ref.current.value;
+    console.log({ username, password1, password2 });
+  };
 
-    return (
-        <h1>This is the Sign up page</h1>
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group className="mb-3" controlId="username">
+        <Form.Label>Username</Form.Label>
+        <Form.Control type="text" placeholder="Enter username" ref={usernameRef} />
+      </Form.Group>
 
-    )
+      <Form.Group className="mb-3" controlId="password1">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" ref={password1Ref} />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="password2">
+        <Form.Label>Confirm password</Form.Label>
+        <Form.Control type="password" placeholder="Password" ref={password2Ref} />
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Sign up
+      </Button>
+    </Form>
+  );
 }
+
 
 export default SignupPage
