bf47b79ae2decf43aa4ae75bbafb663c09577ac8
Anton Askling <anton.askling@gmail.com>
2023-03-13 18:16:01 +0100

    add logout functionality, pass down login logut to value prop, and export

--- src/contexts/authContext.js
+++ src/contexts/authContext.js
@@ -15,17 +15,23 @@ function AuthProvider({ children }) {
     * the login request to dj-rest-auth/login, and stores it in localStorage
     * then it sets the sets the 'isAuthenticated' value to true.
     */
-   
+
     const login = (token) => {
         localStorage.setItem('token', token)
         setIsAuthenticated(true)
     }
 
+    // on logout the authProvider can be set to false. 
+    const logout = () => {
+        localStorage.removeItem('token')
+        setIsAuthenticated(false)
+    }
+
     return (
-        <AuthContext.Provider value={{ isAuthenticated }}>
+        <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
             {children}
         </AuthContext.Provider>
     )
 }
 
-export default AuthProvider
+export {AuthProvider, AuthContext} ;
