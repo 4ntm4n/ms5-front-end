9fa1f41b09cbf0265887241782e426e8f147424c
Anton Askling <anton.askling@gmail.com>
2023-03-13 22:55:50 +0100

    add functionality to get info in authContext and pass it as prop

--- src/contexts/authContext.js
+++ src/contexts/authContext.js
@@ -1,4 +1,5 @@
 import React, { createContext, useState } from 'react'
+import { axiosAuth } from '../api/AxiosDefaults';
 
 
 //create a context object
@@ -8,7 +9,7 @@ const AuthContext = createContext();
 //create an auth Provider to use when wrapping the constext object.
 function AuthProvider({ children }) {
     const [isAuthenticated, setIsAuthenticated] = useState(false)
-
+    const [user, setUser] = useState(null)
     /* 
     *
     * add login functionalty that takes the token recieved as a reponse from
@@ -16,9 +17,16 @@ function AuthProvider({ children }) {
     * then it sets the sets the 'isAuthenticated' value to true.
     */
 
-    const login = (token) => {
+    const login = async (token) => {
         localStorage.setItem('token', token)
         setIsAuthenticated(true)
+
+        try {
+            const { data } = await axiosAuth.get('dj-rest-auth/user/')
+            setUser(data)
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     // on logout the authProvider can be set to false. 
@@ -28,7 +36,7 @@ function AuthProvider({ children }) {
     }
 
     return (
-        <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+        <AuthContext.Provider value={{ isAuthenticated, user, login, logout }}>
             {children}
         </AuthContext.Provider>
     )
