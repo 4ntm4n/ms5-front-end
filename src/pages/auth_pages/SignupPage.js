53f59f799468cabe895866a9f7111b3bd8ed535a
Anton Askling <anton.askling@gmail.com>
2023-03-13 02:47:02 +0100

    refactor signup page to add user to db and navigate user to login on success.

--- src/pages/auth_pages/SignupPage.js
+++ src/pages/auth_pages/SignupPage.js
@@ -1,17 +1,38 @@
 import React, { useRef } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { Form, Button } from 'react-bootstrap'
+import axios from 'axios';
 
 function SignupPage() {
   const usernameRef = useRef(null);
   const password1Ref = useRef(null);
   const password2Ref = useRef(null);
 
-  const handleSubmit = (e) => {
+  const navigate = useNavigate()
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const username = usernameRef.current.value;
     const password1 = password1Ref.current.value;
     const password2 = password2Ref.current.value;
-    console.log({ username, password1, password2 });
+    //console.log({ username, password1, password2 });
+
+    const signupPayLoad = {
+        username,
+        password1,
+        password2,
+    }
+
+    try {
+        const { data } = await axios.post(
+            '/dj-rest-auth/registration/', signupPayLoad
+        )
+        console.log(data)
+        navigate('/login')
+    } catch (error) {
+        console.log(error)
+    }
+
   };
 
   return (
