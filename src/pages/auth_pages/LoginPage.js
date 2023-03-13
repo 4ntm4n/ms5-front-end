925e020212f3f84224fa8f15f20c81d235aa1129
Anton Askling <anton.askling@gmail.com>
2023-03-13 01:58:38 +0100

    add basic login form that logs input to console

--- src/pages/auth_pages/LoginPage.js
+++ src/pages/auth_pages/LoginPage.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { Form, Button } from 'react-bootstrap'
+
 
 function LoginPage() {
+    const usernameRef = useRef(null)
+    const passwordRef = useRef(null)
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const username = usernameRef.current.value;
+        const password = passwordRef.current.value;
+            console.log({ username, password });
+    }
+
     return (
-        <>
-            <h1>This is the Log in page</h1>
-        </>
+        <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="username">
+                <Form.Label>Username</Form.Label>
+                <Form.Control type="text" placeholder="username" ref={usernameRef} />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="username">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="text" placeholder="password" ref={passwordRef} />
+            </Form.Group>
+            <Button variant="dark" type="submit"> Log in </Button>
+        </Form>
     )
 }
 
