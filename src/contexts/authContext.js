4296481e7d3ca9630433a48175d64f78d75ebc29
Anton Askling <anton.askling@gmail.com>
2023-03-13 18:07:03 +0100

    add login functionality to AuthContext Privider

--- src/contexts/authContext.js
+++ src/contexts/authContext.js
@@ -6,14 +6,26 @@ const AuthContext = createContext();
 
 
 //create an auth Provider to use when wrapping the constext object.
-function AuthProvider({children}) {
-    const [isAuthenticated, SetIsAuthenticated] = useState(false)
+function AuthProvider({ children }) {
+    const [isAuthenticated, setIsAuthenticated] = useState(false)
 
-  return (
-    <AuthContext.Provider value={{ isAuthenticated }}>
-        {children}
-    </AuthContext.Provider>
-  )
+    /* 
+    *
+    * add login functionalty that takes the token recieved as a reponse from
+    * the login request to dj-rest-auth/login, and stores it in localStorage
+    * then it sets the sets the 'isAuthenticated' value to true.
+    */
+   
+    const login = (token) => {
+        localStorage.setItem('token', token)
+        setIsAuthenticated(true)
+    }
+
+    return (
+        <AuthContext.Provider value={{ isAuthenticated }}>
+            {children}
+        </AuthContext.Provider>
+    )
 }
 
 export default AuthProvider
