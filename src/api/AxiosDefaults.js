20474116b95f0100e98ca039c08c7ad7daa92052
Anton Askling <anton.askling@gmail.com>
2023-03-13 22:41:53 +0100

    fix: redifine instances of axiosDefaults

--- src/api/AxiosDefaults.js
+++ src/api/AxiosDefaults.js
@@ -4,5 +4,12 @@ axios.defaults.baseURL = "http://localhost:8000/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
+const token = localStorage.getItem('token');
+
 export const axiosReq = axios.create();
-export const axiosRes = axios.create();
+export const axiosAuth = axios.create({
+    headers: {
+        'content-type': 'application/json',
+        'Authorization': `Bearer ${token}`
+    },
+})
