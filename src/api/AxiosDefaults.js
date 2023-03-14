49b934ebfe44d397f629b697698c31d7dd8ecc05
Anton Askling <anton.askling@gmail.com>
2023-03-14 13:56:30 +0100

    update axios Defaults work with simplejwt cookie authentication

--- src/api/AxiosDefaults.js
+++ src/api/AxiosDefaults.js
@@ -2,14 +2,10 @@ import axios from "axios";
 
 axios.defaults.baseURL = "http://localhost:8000/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
-axios.defaults.withCredentials = true;
 
-const token = localStorage.getItem('token');
-
-export const axiosReq = axios.create();
 export const axiosAuth = axios.create({
-    headers: {
-        'content-type': 'application/json',
-        'Authorization': `Bearer ${token}`
-    },
+  withCredentials: true,
 })
+
+//to use for request towards api that do not need authentication header.
+export const axiosReq = axios.create()
--- src/pages/auth_pages/LoginPage.js
+++ src/pages/auth_pages/LoginPage.js
@@ -1,41 +1,54 @@
-import React, { useRef } from 'react'
+import React, { useContext, useRef, useEffect } from 'react'
 import { Form, Button } from 'react-bootstrap'
-import { axiosRes } from '../../api/AxiosDefaults'
-import axios from 'axios'
+import { axiosReq } from '../../api/AxiosDefaults'
+import { AuthContext } from '../../contexts/authContext'
 
 
 function LoginPage() {
-    const usernameRef = useRef(null)
-    const passwordRef = useRef(null)
+  const { login, user } = useContext(AuthContext);
+  const usernameRef = useRef(null)
+  const passwordRef = useRef(null)
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        /* const username = usernameRef.current.value;
-        const password = passwordRef.current.value; */
+  useEffect(() => {
+    console.log(user)
+  }, [user]);
 
-        const loginPayLoad = {username: usernameRef.current.value, password: passwordRef.current.value}
-        try {
-        const { data } = await axiosRes.post('/dj-rest-auth/login/', loginPayLoad)
-        console.log(data)
-        } catch (error) {
-          console.log(error)
-        }
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    /* const username = usernameRef.current.value;
+    const password = passwordRef.current.value; */
+
+    const loginPayLoad = { username: usernameRef.current.value, password: passwordRef.current.value }
+    try {
+      const { data } = await axiosReq.post('/dj-rest-auth/login/', loginPayLoad)
+      console.log("user :", data.user)
+      console.log("access :", data.access_token)
+      console.log("refresh token :", data.refresh_token)
+      // Request to refresh token
+    /*  
+
+      const refreshRes = await axiosReq.post('/dj-rest-auth/token/refresh/', refreshPayLoad)
+      console.log("new token :", refreshRes.data.access) */
+
+    } catch (error) {
+      console.log(error)
     }
+  }
 
-    return (
-        <Form onSubmit={handleSubmit}>
-            <Form.Group className="mb-3" controlId="username">
-                <Form.Label>Username</Form.Label>
-                <Form.Control type="text" placeholder="username" ref={usernameRef} />
-            </Form.Group>
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group className="mb-3" controlId="username">
+        <Form.Label>Username</Form.Label>
+        <Form.Control type="text" placeholder="username" ref={usernameRef} />
+      </Form.Group>
 
-            <Form.Group className="mb-3" controlId="username">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="text" placeholder="password" ref={passwordRef} />
-            </Form.Group>
-            <Button variant="dark" type="submit"> Log in </Button>
-        </Form>
-    )
+      <Form.Group className="mb-3" controlId="username">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="text" placeholder="password" ref={passwordRef} />
+      </Form.Group>
+      <Button variant="dark" type="submit"> Log in </Button>
+    </Form>
+  )
 }
 
 export default LoginPage
