19ae8089e393dda24c20d6a830d54d06d6a320a7
Anton Askling <anton.askling@gmail.com>
2023-03-14 20:39:19 +0100

    handle mount function and useEffect hook

--- src/api/AxiosDefaults.js
+++ src/api/AxiosDefaults.js
@@ -1,11 +1,8 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:8000/";
+axios.defaults.baseURL = "https://drf-api-rec.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+axios.defaults.withCredentials = true;
 
-export const axiosAuth = axios.create({
-  withCredentials: true,
-})
-
-//to use for request towards api that do not need authentication header.
-export const axiosReq = axios.create()
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
--- src/contexts/currentUserContext.js
+++ src/contexts/currentUserContext.js
@@ -1,4 +1,6 @@
-
+import React, { createContext, useContext, useState } from "react";
+import axios from "axios";
+import { axiosRes } from "../api/AxiosDefaults";
 
 export const CurrentUserContext = createContext();
 export const SetCurrentUserContext = createContext();
@@ -6,3 +8,32 @@ export const SetCurrentUserContext = createContext();
 export const useCurrentUser = () => useContext(CurrentUserContext);
 export const useSetCurrentUser = () => useContext(SetCurrentUserContext);
 
+
+
+export const currentUserContext = ({ children }) => {
+    const [currentUser, setCurrentUser] = useState(null)
+
+    // set currentUser state to object returned from dj-rest-auth user.
+    const handleMount = async () => {
+        try {
+            const { data } = await axiosRes.get('dj-rest-auth/user/')
+            setCurrentUser(data);
+        } catch (error) {
+            console.log(error)
+        };
+
+        useEffect(() => {
+            handleMount();
+        }, []);
+
+        return (
+            <CurrentUserContext.Provider>
+                <SetCurrentUserContext.Provider>
+                    {children}
+                </SetCurrentUserContext.Provider>
+            </CurrentUserContext.Provider>
+        )
+    }
+
+
+}
