a822a0d279fa4efd13461e0bf09906692946397d
Anton Askling <anton.askling@gmail.com>
2023-03-16 23:26:19 +0100

    refactor tooltip in  profilePic component

--- src/components/ProfilePic.js
+++ src/components/ProfilePic.js
@@ -3,12 +3,12 @@ import { OverlayTrigger, Tooltip } from 'react-bootstrap'
 import { v4 as uuidv4 } from 'uuid';
 
 
-function ProfilePic({member, size}) {
+function ProfilePic({ member, size }) {
     const picId = uuidv4();
-    const { image, owner} = member
+    const { image, owner } = member
     const picStyle = {
         width: `${size}px`,
-        height:`${size}px`,
+        height: `${size}px`,
         borderRadius: '50%',
         objectFit: 'cover',
         objectPosition: 'center',
@@ -16,17 +16,15 @@ function ProfilePic({member, size}) {
 
     //set tooltip id to the same as user's profile id
     //to make sure it is unique.
-    const showUsername = (
-        <Tooltip id={`tooltip-${picId}`}>
-            {owner}
-        </Tooltip>
+    return (
+        <OverlayTrigger
+            placement="top"
+            delay={{ show: 100, hide: 0 }}
+            overlay={<Tooltip id={`tooltip-${picId}`}>{owner}</Tooltip>}
+        >
+            <img src={image} alt="profile-pic" style={picStyle} />
+        </OverlayTrigger>
     )
-
-  return (
-    <OverlayTrigger placement="top" overlay={showUsername}>
-      <img src={image} alt="profile-pic" style={picStyle} /> 
-    </OverlayTrigger>
-  )
 }
 
 export default ProfilePic
