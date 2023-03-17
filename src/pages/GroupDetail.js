09928f1d7ddad260008715182cac8ac1d1475ea7
Anton Askling <anton.askling@gmail.com>
2023-03-17 07:16:10 +0100

    add GroupDetail logic for fetching data, setLoading state, and render some data.

--- src/pages/GroupDetail.js
+++ src/pages/GroupDetail.js
@@ -1,19 +1,40 @@
+import axios from 'axios'
 import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 import { axiosReq, axiosRes } from '../api/AxiosDefaults'
+import GroupMembers from '../components/GroupMembers';
+import ProfilePic from '../components/ProfilePic';
+
 
 function GroupDetail() {
-
-    useEffect(() => {
+    const { id } = useParams();
+    const [group, setGroup] = useState();
+    const [isLoading, setIsLoading] = useState(true);
+    
+    const fetchGroup = async () => {
         try {
+            const { data } = await axiosReq.get(`/groups/${id}/`)
+            setGroup( data )
+            setIsLoading(false)
+            console.log(data)
         } catch (error) {
             console.log(error)
         }
-    },[])
+    }
+    useEffect(() => {
+        fetchGroup();
+        
+    }, [id]);
+
+
   return (
-    <>
+    <>  
+        
         <h1>Hello from Group Detail Page!</h1>
-        <>Group Owner name</> <br />
-        <>Members component</> <br />
+        {!isLoading && group.group_owner.owner} is my name <br/>
+        {!isLoading && group.name}, is the group name <br/>
+        {!isLoading &&  <GroupMembers group={group} />}  <br/>
+        <> Members component</> <br />
         <>list of tasks components belonging to group</> <br />
         <>add new task to this group functionality to this specific group</> <br />
         <>add remove task to this group functionality </> <br />
