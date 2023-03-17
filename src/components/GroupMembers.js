d97554824da4d0d344d866737caaac134250a6a2
Anton Askling <anton.askling@gmail.com>
2023-03-17 05:16:10 +0100

    add to do list for group member detail, add group member component shell

---
+++ src/components/GroupMembers.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function GroupMembers() {
+  return (
+    <div>GroupMembers</div>
+  )
+}
+
+export default GroupMembers
--- src/pages/GroupDetail.js
+++ src/pages/GroupDetail.js
@@ -1,11 +1,23 @@
-import React from 'react'
-import GroupComponent from '../components/GroupComponent'
+import React, { useState, useEffect } from 'react'
+import { axiosReq, axiosRes } from '../api/AxiosDefaults'
 
 function GroupDetail() {
+
+    useEffect(() => {
+        try {
+        } catch (error) {
+            console.log(error)
+        }
+    },[])
   return (
     <>
         <h1>Hello from Group Detail Page!</h1>
-        <GroupComponent />
+        <>Group Owner name</> <br />
+        <>Members component</> <br />
+        <>list of tasks components belonging to group</> <br />
+        <>add new task to this group functionality to this specific group</> <br />
+        <>add remove task to this group functionality </> <br />
+        <>link to task details</>
         
     </>
   )
