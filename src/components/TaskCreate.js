be871a816c239984867b0dfa832559b27d93db8c
Anton Askling <anton.askling@gmail.com>
2023-03-18 19:57:08 +0100

    adding functionality to set task payload before making request.

--- src/components/TaskCreate.js
+++ src/components/TaskCreate.js
@@ -1,11 +1,14 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState, useEffect } from 'react'
+import { Button } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 import { useCurrentUser } from '../contexts/currentUserContext'
 
 function TaskCreate() {
     const currentUser = useCurrentUser();
-    const [taskPayload, setTaskPayload] = useState();
-    const title = useRef('');
-    const description = useRef('');
+    const titleRef = useRef(null);
+    const descriptionRef = useRef(null);
+    const [payLoad, setPayload] = useState(null)
+    const navigate = useNavigate();
     /* 
        enpoint: /tasks/create/
        accepts:
@@ -24,12 +27,37 @@ function TaskCreate() {
               a later stage.
     } */
 
-    const handleCreate = () => {
-        
+    const handleCreate = (e) => {
+        e.preventDefault();
+        const taskPayload = {
+          owning_group: "set this next",
+          title: titleRef.current.value,
+          description: descriptionRef.current.value,
+        }
+        setPayload(taskPayload)
+        console.log(taskPayload)
+        navigate("/")
     }
 
+    useEffect(() => {
+      
+    }, [payLoad]);
+
     return (
-    <div>TaskCreate</div>
+    <>
+    
+    <form onSubmit={handleCreate}>
+      <label for="taskTitle" >Task Title: </label>
+      <input placeholder="task title" id="taskTitle" name="taskTitle" ref={titleRef}></input> <br/>
+
+      <label for="taskDescription">Task desc:</label>
+      <textArea placeholder="task description" id="taskDescription" name="taskDescription" ref={descriptionRef}></textArea> <br />
+      <Button type="submit">add task</Button>
+    </form>
+
+      <p> payload: {payLoad? (<>{payLoad.title} <br /> {payLoad.description} </>):("no payload yet")}</p>
+    </>
+
   )
 }
 
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import TaskCreate from '../components/TaskCreate'
 
 function HomePage() {
   return (
     <>
       <h1>Hello from HomePage</h1>
+      <TaskCreate></TaskCreate>
     </>
   )
 }
