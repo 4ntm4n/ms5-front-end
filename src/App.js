087d9a33a3f7d6f96910024afe703f792d2a5eeb
Anton Askling <anton.askling@gmail.com>
2023-03-19 16:28:48 +0100

    Wrap TaskProvider around protected routes.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './App.css';
 //import CustomButton from './components/CustomButton';
 import NavBar from './components/NavBar';
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, outlet, Outlet } from 'react-router-dom'
 import FeedPage from './pages/FeedPage';
 import HomePage from './pages/HomePage';
 import GroupsPage from './pages/GroupsPage';
@@ -12,7 +12,7 @@ import LoginPage from './pages/auth_pages/LoginPage';
 import SignupPage from './pages/auth_pages/SignupPage';
 import NotFound from './pages/error-pages/NotFound';
 import GroupDetail from './pages/GroupDetail';
-
+import { TaskProvider } from './contexts/TaskContext';
 
 
 
@@ -29,12 +29,17 @@ function App() {
           <Route path='/signup' element={<SignupPage />} />
 
           {/* these paths needs to be protected paths, "if auth." */}
-
-          <Route path='/feed' element={<FeedPage />} />
-          <Route path='/groups' element={<GroupsPage />} />
-          <Route path='/groups/:id' element={<GroupDetail />} />
-          <Route path='/tasks' element={<TasksPage />} />
-          
+          {/* adding route to wrap around protected route, use "/" parent route */}
+          <Route path="/" element={
+            <TaskProvider> 
+              <Outlet />  {/* wrapping TaskProvider around outlet since route has to be 1st child of Router. */}
+            </TaskProvider>
+            } >
+            <Route path='/groups' element={<GroupsPage />} />
+            <Route path='/groups/:id' element={<GroupDetail />} />
+            <Route path='/feed' element={<FeedPage />} />            
+            <Route path='/tasks' element={<TasksPage />} />
+          </Route>
         </Routes>
       </div>
   );
