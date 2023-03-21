ecfbfd876bcc5a2a0882552f13f1d09a0e2c8bd8
Anton Askling <anton.askling@gmail.com>
2023-03-21 19:26:00 +0100

    add tasks state to tasks context

--- src/contexts/TasksContext.js
+++ src/contexts/TasksContext.js
@@ -7,8 +7,11 @@ export const useTasks = () => useContext(TasksContext)
 export const useTasksUpdate = () => useContext(TasksUpdateContext)
 
 export const TasksProvider = ({ children }) => {
+    //state to store tasks globally and not fetch from the server every time
+    const [tasks, setTasks] = useState([]);
     //to toggle when task is updating and let parent components know
     const [tasksUpdate, setTasksUpdate] = useState(true)
+    
 
 
     const toggleUpdate = () => {
