08c7ea071c71e4ba08545785e407dbb704b80b37
Anton Askling <anton.askling@gmail.com>
2023-03-22 03:40:15 +0100

    add fetching of data on change ProfileSearch.

--- src/components/ProfileSearch.js
+++ src/components/ProfileSearch.js
@@ -1,25 +1,37 @@
 import React, { useState, useEffect } from 'react'
+import { axiosReq } from '../api/AxiosDefaults'
 import ProfilePic from './ProfilePic'
+
+
 
 function ProfileSearch() {
 const [profiles, setProfiles] = useState([])
 const [searchField, setSearchField] = useState("")
 
 const handleSearch = (e) => {
-    setSearchField(e.current.value)
+    setSearchField(e.currentTarget.value)
+}
+
+const fetchProfiles = async () => {
+    try {
+        const { data } = await axiosReq.get(`/profiles/?search=${searchField}`)
+        console.log(data.results)
+    } catch (error) {
+        
+    }
 }
 
 useEffect(() => {
-    
+    fetchProfiles()
 }, [searchField]);
 return (
     <div>
-        <input onChange={(e) => handleSearch} ></input>
+        <input onChange={(e) => handleSearch(e)} ></input>
         <ul>
             {profiles.length? (
                 profiles.map( profile => (
-                    <li>
-                        
+                    <li key={profile.id}>
+                        {profile}
                     </li>
                 ))
             ) : ("search for a profile...")}
