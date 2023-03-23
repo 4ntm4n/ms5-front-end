107440aa36f539b5e9ea7ef010ce337f567a76ad
Anton Askling <anton.askling@gmail.com>
2023-03-23 01:11:04 +0100

    add functions that triggers when add or remove button clicked.

--- src/components/ProfileSearch.js
+++ src/components/ProfileSearch.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import { axiosReq } from '../api/AxiosDefaults'
 import ProfilePic from './ProfilePic'
-
+import { useTasksUpdate } from '../contexts/TasksContext'
 
 
 function ProfileSearch({ groupObj , groupId }) {
 const [profiles, setProfiles] = useState([])
 const [searchField, setSearchField] = useState(null)
-
+const updateTasksList = useTasksUpdate()
 const handleSearch = (e) => {
     setSearchField(e.currentTarget.value)
 }
@@ -21,6 +21,11 @@ const fetchProfiles = async () => {
     }
 }
 
+const handleAddRemove = (id) => {
+    console.log(id)
+    setSearchField("")
+}
+
 useEffect(() => {
     if (searchField !== ""){
         fetchProfiles()
@@ -29,6 +34,8 @@ useEffect(() => {
         setSearchField(null)
     }
 }, [searchField]);
+
+
 return (
     <div>
     <input onChange={handleSearch} ></input>
@@ -44,20 +51,21 @@ return (
              * with each itteration of the profiles map that we are inside right now.
              * if the profile.owner matches any  member.owner, it means that the profile, is already
              * in the members list of the group. and we add a remove button, else we render an add button. 
-             * Note. it will only return any button at all if its not the group owner, cuz group owner can not
-             * remove itself from the group on the server side...
+             * Note. it will only return a the profile if the profile is not group owner, cuz group owners can't
+             * remove itself from the group on the server side in this app...
             */
             const memberMatch = groupObj.members.find(member => member.owner === profile.owner);
             const groupOwner = groupObj.group_owner.owner
             
+            //exclude from searchresults if profile is group owner.
             if (profile.owner === groupOwner) {
                 return null; 
               }
             return  (
                 <li key ={profile.id} >
                     {profile.owner}
-                    {memberMatch && <button>remove</button>}
-                    {!memberMatch && <button>add</button>}
+                    {memberMatch &&  <button onClick={() => handleAddRemove(profile.id)}>remove</button>}
+                    {!memberMatch && <button onClick={() => handleAddRemove(profile.id)}>add</button>}
                 </li>
                 
             )
