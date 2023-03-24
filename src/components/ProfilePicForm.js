5cd51404b56eb5a30160e0c28884e6276ae6c13c
Anton Askling <anton.askling@gmail.com>
2023-03-24 02:23:22 +0100

    add render image preview so user can se profile pic before uploading.

--- src/components/ProfilePicForm.js
+++ src/components/ProfilePicForm.js
@@ -20,6 +20,13 @@ function ProfilePicForm() {
         }));
     }
 
+    const renderImagePreview = () => {
+        if (!profilePayLoad.image) return null;
+
+        const imageUrl = URL.createObjectURL(profilePayLoad.image)
+        return <img src={imageUrl} alt="profile image preview" style={{width: '120px', height: '120px'}} />
+    }
+
     const handleUpload = () => {
         console.log("form trying to server")
     }
@@ -27,7 +34,7 @@ function ProfilePicForm() {
 
   return (
     <div>
-        <div onClick={triggerImgChoice} className={styles.UploadContainer}>red square</div>
+        <div onClick={triggerImgChoice} className={styles.UploadContainer}>{profilePayLoad.image && renderImagePreview()}</div>
         <Form.Group className="mb-3" controlId="image-upload">
           <Form.Label>Task Title</Form.Label>
           <Form.Control
